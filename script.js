9e77337e610df59a2fa3ba8a70a319cd5c973b7a
giuseppezaccato <giuseppezaccato@gmail.com>
2025-01-23 19:11:54 +0100

    END: form + mq

--- script.js
+++ script.js
@@ -36,30 +36,68 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
-const container = document.querySelector('.container');
 
-for (let i = 0; i < teamMembers.length; i++) {
+//task seleziono campo in cui scrivere
+const teams = document.getElementById('teams');
 
-  let card = teamMembers[i];
+//task creo funzione per fillare in maniera automatica con un ciclo e il template literal 
+function addCards() {
+  for (let i = 0; i < teamMembers.length; i++) {
 
-  container.innerHTML += `
-        <div>
-            <div class="d-flex align-items-center pb-5 justify-content-center  " style="width: 18rem;">
-                <img src="${card.img}" alt="${card.name}">
-                <div>
-                    <ul class="list-group text-center list-group-flush">
-                        <li class="list-group-item"> <h5>${card.name}</h5> </li>
-                        <li class="list-group-item">${card.role} </li>
-                        <li class="list-group-item text-primary">${card.email}</li>
-                    </ul>
+    let card = teamMembers[i];
+
+    teams.innerHTML += `
+            <div>
+                <div class="card mb-4 " style="width: 18rem;">
+                    <img src="${card.img}" alt="${card.name}">
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush text-center ">
+                            <li class="list-group-item">
+                                <h5>${card.name}</h5>
+                            </li>
+                            <li class="list-group-item">${card.role} </li>
+                            <li class="list-group-item text-primary">${card.email}</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-
-        </div>
     `
 
+  }
+};
 
+//task invoco la funzione
+addCards();
 
+//task eventlistener e selezione submit
+const sub = document.getElementById("sub");
 
+sub.addEventListener("click", function (event) {
 
-}
+  event.preventDefault();
+
+  //task collect values di tutti i campi input
+  const name = document.getElementById("name").value;
+  const role = document.getElementById("role").value;
+  const email = document.getElementById("email").value;
+  const img = document.getElementById("img").value;
+
+  //task push nell'array del nuovo obj con tutte le chiavi corrispondenti
+  teamMembers.push({
+    name,
+    role,
+    email,
+    img
+  });
+
+  console.log(teamMembers);
+
+  //task svuoto il campo teams
+  teams.innerHTML = '';
+
+  //task re-invoco la funzione per aggiungere anche la card appena creata col FORM
+  //* NB il form autocompila richiamando le chiavi inserite con l'input,
+  //*  grazie al ${} nel template literal
+  addCards();
+});
+
